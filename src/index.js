24cdefda5577323667da564054bc8366a452f81b
Subhash <Subhash613d@gmail.com>
2024-02-12 19:45:56 +0530

    routes optimisation

--- src/index.js
+++ src/index.js
@@ -16,21 +16,12 @@ app.listen(PORT, () => {
 });
 
 // Use path.join() to get the absolute path to index.html
-app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, 'index.html'));
-});
 app.get('/home', (req, res) => {
     return res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/styles.css', (req, res) => {
-    res.set('Content-Type', 'text/css');
-    res.sendFile(path.join(__dirname,'styles.css'));
+
+app.get('/*', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/index.js', (req, res) => {
-    res.set('Content-Type', 'text/javascript');
-    res.sendFile(path.join(__dirname, 'index.js'));
-});
-app.get('/my_image', (req, res) => {
-    res.set('Content-Type', 'image/jpeg');
-    res.sendFile(path.join(__dirname, 'my_image.jpg'));
-});
+
+app.use(express.static(path.join(__dirname, 'src')));
